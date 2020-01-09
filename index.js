8343c41cffff9625b5ab2af4b725bca920d395ff
Mary Varnado <myvarnado@gmail.com>
2020-01-09 08:22:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+beforeAll()
 function sayHiToGrandma(string) {
   if (console.log === string.toLowerCase()) {
     console.log "I can't hear you."
