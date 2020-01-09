3c2fb9409e45ae76f8ae2c851b3f086d0deaddc5
Mary Varnado <myvarnado@gmail.com>
2020-01-09 08:21:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,8 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (console.log(string.toLowerCase())){
-    console.log "I can't hear you!"
-  } else if (console.log(string.toUpperCase())){
-    console.log "YES INDEED!" }
-    else if ( string = "I love you, Grandma.")
-  {console.log "I love you, too."
-    }
+  if (console.log === string.toLowerCase()) {
+    console.log "I can't hear you." 
+  } else if (console.log === string.toUpperCase()) {
+    console.log "YES INDEED!"
   }
